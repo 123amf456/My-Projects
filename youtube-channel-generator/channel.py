b41675c8448feb08e8acbb16c9170acd9cb3a881
Abdulrahman <123amf45@gmail.com>
2025-05-15 22:57:48 +0300

    YouTube channel name generator
    The program asks the user for their name and the channel's topic,
    then generates a suggested channel name combining both inputs.

---
+++ youtube-channel-generator/channel.py
@@ -0,0 +1,4 @@
+print("Welcome to YouTube channel generator :")
+name=input("What is your name?\n")
+channel=input("What is your channelabout?\n")
+print("You can name your channel : "+(channel)+(" with ")+(name))
